5d00e6d48e16869c19be625aa45853c4c8c984ff
Enddong <90543799+Enddong@users.noreply.github.com>
2023-09-18 08:57:46 +0900

    0915NameTag

---
+++ App.tsx
@@ -0,0 +1,17 @@
+import React from 'react';
+import Hello from './Hello';
+import Wrapper from './Wrapper';
+import styled from 'styled-components';
+import MyComponent from './MyComponent';
+
+function App(){
+
+  return (
+    
+    <MyComponent firstname="Eunchae" lastname="KIM"/>
+    
+  );
+
+}
+
+export default App;
